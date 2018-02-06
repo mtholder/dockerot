4b403be1eb460f70e6548a9fd2a7ca68c4893a65
Mark T. Holder <mtholder@gmail.com>
2018-02-06 10:55:08 -0600

    not looking promising without compose

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build --target otbuildenv -t otbuildenv otbuildenv || exit
 docker build --target restbedbuilder -t restbedbuilder otcws || exit
-docker build --target otcfreshbuilder -t otcfreshbuilder otcws || exit
+docker build --target otcbuilder -t otcbuilder --force-rm otcws || exit
 bash refresh-otcws.sh || exit
--- refresh-otcws.sh
+++ refresh-otcws.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker build --target otcbuilder -t otcbuilder otcws || exit
 docker build --target otcws -t otcws otcws || exit
